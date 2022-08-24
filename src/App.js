bf1c3989e5df8f6c36c565166842a93e1fcc378a
Emir Al <mythia@windowslive.com>
2022-08-24 15:25:47 +0300

    feat: add About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/navbar/Navbar';
+import About from './components/pages/About';
 import Home from './components/pages/home/Home';
 import ProductPage from './components/pages/product/ProductPage';
 import ProductContextProvider from './contexts/ProductContext';
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/product/:productId' element={<ProductPage />} />
+          <Route path='/about' element={<About />} />
         </Routes>
       </ProductContextProvider>
       <Footer />
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,45 @@
+import Container from '../shared/Container';
+
+const About = () => {
+  return (
+    <Container>
+      <h1 className='font-bold text-2xl mb-2'>About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti quasi
+        debitis, ratione repellendus fugiat quisquam ipsam? Vitae dolorem quo
+        dolorum, minima dolore modi nulla eius alias, ad ipsam veritatis
+        voluptate. Recusandae tempora pariatur, ab eligendi, quasi saepe debitis
+        ut autem quod accusantium optio a! Laboriosam veniam modi dicta, soluta
+        excepturi voluptate nostrum libero odit praesentium consequuntur
+        dignissimos sed, commodi minus? Natus ratione laborum quam hic dolor
+        deserunt eius officia asperiores ipsa pariatur? Officiis, earum
+        eligendi. Obcaecati molestias, atque dicta, dolores rem, ut asperiores
+        recusandae perspiciatis saepe velit nostrum esse odio? Aut explicabo
+        harum soluta. Eveniet consequatur assumenda, quo, corrupti recusandae
+        incidunt modi illo amet pariatur sapiente expedita autem aliquam quis
+        voluptate cupiditate odit vel necessitatibus quas? Veritatis aliquid
+        suscipit sit. Corporis amet sit impedit ut maxime iste ab, voluptatem
+        repudiandae pariatur placeat deserunt consectetur commodi! Amet,
+        deserunt? Nulla rerum accusantium mollitia sint doloribus? Aperiam
+        delectus, labore dolor magnam accusamus odio? Deserunt dignissimos at
+        animi, ullam voluptate laborum odio id? Sint natus, excepturi quaerat,
+        odit minus facere ratione quas incidunt repudiandae sit voluptatum
+        numquam debitis libero quibusdam consequuntur nihil, blanditiis minima.
+        Consectetur ipsam quos amet inventore exercitationem ea ab itaque
+        officiis, modi mollitia numquam rem doloremque sint beatae distinctio
+        quas suscipit quam ut laborum nisi. Rem corrupti iure obcaecati
+        voluptatem sequi? Nesciunt molestias nulla veniam dicta iure. Ducimus
+        odit illo doloremque nulla fugit, repellat excepturi natus explicabo
+        molestias ex eaque fuga itaque? Doloribus velit fugiat exercitationem
+        odio accusantium voluptate dignissimos placeat. Labore placeat nisi,
+        veritatis vero facere iusto! Distinctio vero nulla dolorum aliquam.
+        Ducimus, quasi aut officiis consequatur tempora neque, odit quae eius
+        expedita excepturi obcaecati nobis. Enim cupiditate quaerat assumenda!
+        Amet unde laudantium libero veniam nihil dicta odio sed iste minima,
+        obcaecati cum ea perspiciatis, ut incidunt, eos qui tenetur ex dolorum
+        suscipit voluptates quia quas? Obcaecati, illum omnis. Et.
+      </p>
+    </Container>
+  );
+};
+export default About;
