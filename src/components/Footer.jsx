5d56e4830d98f597cde985dde9edc03ed6cd0177
Emir Al <mythia@windowslive.com>
2022-08-24 13:27:20 +0300

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className='w-full h-14 bg-slate-900 text-white flex items-center p-4'>
+      Lorem ipsum dolor sit amet consectetur.
+    </footer>
+  );
+};
+export default Footer;
