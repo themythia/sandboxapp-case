cce9acd9e60302050f5ef7c77063351d77ada092
Emir Al <mythia@windowslive.com>
2022-08-24 13:27:59 +0300

    feat: add ProductContext context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/pages/Home';
+import ProductContextProvider from './contexts/ProductContext';
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <ProductContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </ProductContextProvider>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/contexts/ProductContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useState } from 'react';
+
+export const ProductContext = createContext();
+
+const ProductContextProvider = ({ children }) => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    if (!products) {
+      fetch('https://dummyjson.com/products')
+        .then((res) => res.json())
+        .then((data) => setProducts(data.products))
+        .catch((e) => console.warn(e));
+    }
+  }, [products]);
+
+  return (
+    <ProductContext.Provider value={{ products, setProducts }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductContextProvider;
